a26e44d41b8d6e804f5c4da90fd5816653a5f3aa
brwinner5350 <ogermain2007@yahoo.com>
2022-05-22 23:24:53 -0500

    Minimal codegen This commit introduces the beginning of it all. There is no parsing, there is no lexer, it's all done in C++.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+// main.cpp
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
 
-int main()
-{
-    std::cout << "Hello, borgerlang!" << std::endl;
+int main() {
+    llvm::LLVMContext context;
+
+    std::unique_ptr<llvm::Module> mod;
+    mod = std::make_unique<llvm::Module>("Borgerlang module", context);
+
+    // Build the IR for this block
+    llvm::IRBuilder<> builder(context);
+
+    // Main returns void
+    llvm::FunctionType *mainReturnType = 
+        llvm::FunctionType::get(llvm::Type::getVoidTy(context), false);
+
+    // The main function
+    llvm::Function *mainFunction =
+        llvm::Function::Create(mainReturnType, llvm::Function::ExternalLinkage, "borger_entry", mod.get()); // TODO: change this to main later on
+
+    // Create the function's body
+    llvm::BasicBlock *body = llvm::BasicBlock::Create(context, "body", mainFunction);
+    builder.SetInsertPoint(body);
+
+    builder.CreateRetVoid();
+    mod->print(llvm::outs(), nullptr);
+
     return 0;
 }
